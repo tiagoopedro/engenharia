f945e0b806c1d9aa72799c2fa99070c7f33c6406
tiagoopedro <c.tiagopedro@hotmail.com>
2025-04-08 16:54:22 -0300

    a

--- js/script.js
+++ js/script.js
@@ -40,6 +40,10 @@ function clickMenu(perfil) {
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
