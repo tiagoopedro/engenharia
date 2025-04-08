289721d8ef34b0ca57889f3dae95bc12870fee6f
tiagoopedro <c.tiagopedro@hotmail.com>
2025-04-08 16:13:50 -0300

    a

--- js/script.js
+++ js/script.js
@@ -36,3 +36,26 @@ function clickMenu(perfil) {
         perfil.style.display = 'block'
     } 
 }
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    },
+  });
