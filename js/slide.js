e678f43977e9021be2c9bda9b6912c0d069e4c77
tiagoopedro <c.tiagopedro@hotmail.com>
2025-04-15 10:38:16 -0300

    a

--- js/slide.js
+++ js/slide.js
@@ -31,9 +31,36 @@
     })}}
 
   numeroSlide()
- 
 
-  
+ // codigo do menu lateral do header
+  var menuButton = document.querySelector('.menu-button');
+    var openMenu = function () {
+      swiper.slidePrev();
+    };
+    var swiper = new Swiper('.burguer', {
+      slidesPerView: 'auto',
+      initialSlide: 1,
+      resistanceRatio: 0,
+      slideToClickedSlide: true,
+      on: {
+        slideChangeTransitionStart: function () {
+          var slider = this;
+          if (slider.activeIndex === 0) {
+            menuButton.classList.add('cross');
+            // required because of slideToClickedSlide
+            menuButton.removeEventListener('click', openMenu, true);
+          } else {
+            menuButton.classList.remove('cross');
+          }
+        },
+        slideChangeTransitionEnd: function () {
+          var slider = this;
+          if (slider.activeIndex === 1) {
+            menuButton.addEventListener('click', openMenu, true);
+          }
+        },
+      },
+    });
  
   
  
