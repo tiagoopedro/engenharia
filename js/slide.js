9aef07bbf9dee01d7c1a331410ed717996336f46
tiagoopedro <c.tiagopedro@hotmail.com>
2025-04-24 13:55:59 -0300

    a

--- js/slide.js
+++ js/slide.js
@@ -1,19 +1,34 @@
 //serviços a disposicao
-function numeroSlide() { 
-    if (window.innerWidth >= 768) {
-   var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
-    spaceBetween: 40,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  })}
-  else { 
+function numeroSlide() {
+  if (window.innerWidth >= 2000) {
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 4,
+      spaceBetween: 40,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    })
+  }
+  else if (window.innerWidth >= 768) {
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 40,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    })
+  }
+  else {
     var swiper = new Swiper(".mySwiper", {
       slidesPerView: 1,
       spaceBetween: 40,
@@ -29,40 +44,42 @@ function numeroSlide() {
         delay: 3000,
         disableOnInteraction: false,
       },
-    })}}
+    })
+  }
+}
 
-  numeroSlide()
+numeroSlide()
 
- // codigo do menu lateral do header
- var menuButton = document.querySelector('.menu-button');
-    var openMenu = function () {
-      swiper.slidePrev();
-    };
-    var swiper = new Swiper('.burguer', {
-      slidesPerView: 'auto',
-      initialSlide: 1,
-      resistanceRatio: 0,
-      slideToClickedSlide: true,
-      on: {
-        slideChangeTransitionStart: function () {
-          var slider = this;
-          if (slider.activeIndex === 0) {
-            menuButton.classList.add('cross');
-            // required because of slideToClickedSlide
-            menuButton.removeEventListener('click', openMenu, true);
-          } else {
-            menuButton.classList.remove('cross');
-          }
-        },
-        slideChangeTransitionEnd: function () {
-          var slider = this;
-          if (slider.activeIndex === 1) {
-            menuButton.addEventListener('click', openMenu, true);
-          }
-        },
-      },
-    });
- 
+// codigo do menu lateral do header
+var menuButton = document.querySelector('.menu-button');
+var openMenu = function () {
+  swiper.slidePrev();
+};
+var swiper = new Swiper('.burguer', {
+  slidesPerView: 'auto',
+  initialSlide: 1,
+  resistanceRatio: 0,
+  slideToClickedSlide: true,
+  on: {
+    slideChangeTransitionStart: function () {
+      var slider = this;
+      if (slider.activeIndex === 0) {
+        menuButton.classList.add('cross');
+        // required because of slideToClickedSlide
+        menuButton.removeEventListener('click', openMenu, true);
+      } else {
+        menuButton.classList.remove('cross');
+      }
+    },
+    slideChangeTransitionEnd: function () {
+      var slider = this;
+      if (slider.activeIndex === 1) {
+        menuButton.addEventListener('click', openMenu, true);
+      }
+    },
+  },
+});
+
 
 //slide inicial historico de serviços 
 var swiper = new Swiper(".mySwiper-slide-historico", {
@@ -70,18 +87,18 @@ var swiper = new Swiper(".mySwiper-slide-historico", {
   spaceBetween: 30,
   loop: true,
   pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    el: ".swiper-pagination",
+    clickable: true,
   },
   navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
+    delay: 5000,
+    disableOnInteraction: false,
   },
 });
 
- 
+
 
