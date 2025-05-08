18d52c9f430db3e7b7f4385ab1f0ba1b910c1a19
tiagoopedro <c.tiagopedro@hotmail.com>
2025-05-08 16:03:51 -0300

    Update slide.js

--- js/slide.js
+++ js/slide.js
@@ -30,7 +30,10 @@ var swiperm = new Swiper(".mySwiper-slide-mobile", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
   }, 
-  
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
 });
 
 //serviços a disposicao
