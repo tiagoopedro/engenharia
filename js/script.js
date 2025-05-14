a4fc6de82f4d749b8d78543021a0a0c18b7fd048
tiagoopedro <c.tiagopedro@hotmail.com>
2025-05-14 12:07:26 -0300

    a

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 function mudouTamanho() { 
-    if (window.innerWidth >= 768) {
+    if (window.innerWidth >= 992) {
         itens.style.display = 'block'
     } else {
         itens.style.display = 'none'
@@ -15,3 +15,24 @@ function clickMenu(perfil) {
         perfil.style.display = 'block'
     } 
 }
+
+
+ /* funcao da animacao */
+ const target = document.querySelectorAll('[data-anime]');
+ const animationClass = 'animate';
+
+ function animeScroll() {
+    const windowTop = window.pageYOffset + 1100;
+    console.log(windowTop);
+    target.forEach(function(element) {
+        if(windowTop > element.offsetTop) {
+            element.classList.add(animationClass) //'animate' ou a const como foi inserido
+        }
+
+        console.log(element.offsetTop);
+    })
+ }
+
+ window.addEventListener('scroll', function() {
+    animeScroll();
+ })
