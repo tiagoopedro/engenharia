4e414c289e4b0722a5118f72a1e53ac20dd916dc
tiagoopedro <c.tiagopedro@hotmail.com>
2025-05-14 18:11:30 -0300

    a

--- js/slide.js
+++ js/slide.js
@@ -131,7 +131,7 @@ var swiper = new Swiper('.burguer', {
   },
 });
 
-var swiper = new Swiper(".mySwiper-mobile", {
+ var slideMobile = new Swiper(".mySwiper-slide-mobile", {
       pagination: {
         el: ".swiper-pagination",
       },
